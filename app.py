5967e52d9d46d9c12aea1ee9756842a900b67db4
4l3xisgo2573 <110707335+4l3xisgo2573@users.noreply.github.com>
2023-09-14 11:46:41 -0500

    changes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
