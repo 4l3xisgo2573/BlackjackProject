cda711b8b50342a8f3753863baba021fe140d4fb
4l3xisgo2573 <110707335+4l3xisgo2573@users.noreply.github.com>
2023-10-02 20:52:40 -0500

    changes for frontend and some python

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, request
+import cardDictionary
 app = Flask(__name__)
 
+counter = 0
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global counter 
+    if request.method == 'POST':
+        if request.form['action'] == 'yes':
+            counter += 1
+            print()
+            print(cardDictionary.cardDB[1]["value"])
+            print()
+        elif request.form['action'] == 'no':
+            counter -= 1
+    
+    return render_template('index.html', counter=counter)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
---
+++ cardDictionary.py
@@ -0,0 +1,13 @@
+cardDB = {
+    1:{
+        "cardImg":"",
+        "value":10,
+    },
+    2:{
+        "cardImg":"",
+        "value":20,
+    }
+}
+usedCards ={
+    
+}
