34b6db605d56091b473f63e7ea03599181e58915
4l3xisgo2573 <110707335+4l3xisgo2573@users.noreply.github.com>
2023-10-10 23:38:39 -0500

    added DB and some python

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 from flask import Flask, render_template, request
-import cardDictionary
+import random
+from cardDictionary import cardDB
+
 app = Flask(__name__)
 
+
+
+
+chosenCard = cardDB[1]["cardImg"]
+
 counter = 0
+botOne={}
+botTwo={}
+userPlayer={}
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    global counter 
-    if request.method == 'POST':
-        if request.form['action'] == 'yes':
-            counter += 1
-            print()
-            print(cardDictionary.cardDB[1]["value"])
-            print()
-        elif request.form['action'] == 'no':
-            counter -= 1
+    global counter,botOne,botTwo,userPlayer
     
-    return render_template('index.html', counter=counter)
+    
+    if request.method == "POST":
+        if request.form["action"] == "yes":
+            
+            randomCard = random.randint(1,len(cardDB))
+            print(randomCard)
+            counter += cardDB[randomCard]['value']
+            print(f"Chosen Card Value: {cardDB[randomCard]['value']}")
+        elif request.form["action"] == "no":
+            counter -= 1
 
-if __name__ == '__main__':
+    return render_template("index.html", counter=counter, chosenCard=chosenCard)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- cardDictionary.py
+++ cardDictionary.py
@@ -1,13 +1,162 @@
 cardDB = {
-    1:{
-        "cardImg":"",
-        "value":10,
+    1: {
+        "cardImg": "static/images/2_Club.png",
+        "value": 2,
     },
-    2:{
-        "cardImg":"",
-        "value":20,
-    }
-}
-usedCards ={
-    
+    2: {
+        "cardImg": "static/images/2_dia.png",
+        "value": 2,
+    },
+    3: {
+        "cardImg": "static/images/2_hea.png",
+        "value": 2,
+    },
+    4: {
+        "cardImg": "static/images/2_spa.png",
+        "value": 2,
+    },
+    5: {
+        "cardImg": "static/images/3_Club.png",
+        "value": 3,
+    },
+    6: {
+        "cardImg": "static/images/3_dia.png",
+        "value": 3,
+    },
+    7: {
+        "cardImg": "static/images/3_hea.png",
+        "value": 3,
+    },
+    8: {
+        "cardImg": "static/images/3_spa.png",
+        "value": 3,
+    },
+    9: {
+        "cardImg": "static/images/4_Club.png",
+        "value": 4,
+    },
+    10: {
+        "cardImg": "static/images/4_dia.png",
+        "value": 4,
+    },
+    11: {
+        "cardImg": "static/images/4_hea.png",
+        "value": 4,
+    },
+    12: {
+        "cardImg": "static/images/4_spa.png",
+        "value": 4,
+    },
+    13: {
+        "cardImg": "static/images/5_club.png",
+        "value": 5,
+    },
+    14: {
+        "cardImg": "static/images/5_dia.png",
+        "value": 5,
+    },
+    15: {
+        "cardImg": "static/images/5_hea.png",
+        "value": 5,
+    },
+    16: {
+        "cardImg": "static/images/5_spa.png",
+        "value": 5,
+    },
+    17: {
+        "cardImg": "static/images/6_CLub.png",
+        "value": 6,
+    },
+    18: {
+        "cardImg": "static/images/6_dia.png",
+        "value": 6,
+    },
+    19: {
+        "cardImg": "static/images/6_hea.png",
+        "value": 6,
+    },
+    20: {
+        "cardImg": "static/images/6_spa.png",
+        "value": 6,
+    },
+    21: {
+        "cardImg": "static/images/7_club.png",
+        "value": 7,
+    },
+    22: {
+        "cardImg": "static/images/7_dia.png",
+        "value": 7,
+    },
+    23: {
+        "cardImg": "static/images/7_hea.png",
+        "value": 7,
+    },
+    24: {
+        "cardImg": "static/images/7_spa.png",
+        "value": 7,
+    },
+    25: {
+        "cardImg": "static/images/8_Club.png",
+        "value": 8,
+    },
+    26: {
+        "cardImg": "static/images/8_dia.png",
+        "value": 8,
+    },
+    27: {
+        "cardImg": "static/images/8_hea.png",
+        "value": 8,
+    },
+    28: {
+        "cardImg": "static/images/8_spa.png",
+        "value": 8,
+    },
+    29: {
+        "cardImg": "static/images/9_Club.png",
+        "value": 9,
+    },
+    30: {
+        "cardImg": "static/images/9_dia.png",
+        "value": 9,
+    },
+    31: {
+        "cardImg": "static/images/9_hea.png",
+        "value": 9,
+    },
+    32: {
+        "cardImg": "static/images/9_spa.png",
+        "value": 9,
+    },
+    33: {
+        "cardImg": "static/images/10_Club.png",
+        "value": 10,
+    },
+    34: {
+        "cardImg": "static/images/10_dia.png",
+        "value": 10,
+    },
+    35: {
+        "cardImg": "static/images/10_hea.png",
+        "value": 10,
+    },
+    36: {
+        "cardImg": "static/images/10_spa.png",
+        "value": 10,
+    },
+    37: {
+        "cardImg": "static/images/A_dia.png",
+        "value": 1,
+    },
+    38: {
+        "cardImg": "static/images/A_hea.png",
+        "value": 1,
+    },
+    39: {
+        "cardImg": "static/images/A_spa.png",
+        "value": 1,
+    },
+    40: {
+        "cardImg": "static/images/Ace_Club.png",
+        "value": 1,
+    },
 }
